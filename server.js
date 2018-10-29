c922bfefb3f29f95ce12e4a3e2e9a39a13ffad77
Sudhakar Mishra <sudhakar.strongmoments@gmail.com>
2018-10-29 14:57:38 +0530

    Add DELETE todos route

--- server.js
+++ server.js
@@ -33,10 +33,22 @@ app.post('/todos',function(req, res){
         return res.status(400).send();
     }
     body.description = body.description.trim();
-    
+
     body.id = todoNextId++;
     todos.push(body);
     res.json(body);
+});
+
+app.delete('/todos/:id', function (req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (!matchedTodo) {
+        res.status(404).json({"error":"no todo found with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function(){
