34967e62686f4af770e0ab2b86e51fab928cae8e
Sudhakar Mishra <sudhakar.strongmoments@gmail.com>
2018-10-30 15:40:27 +0530

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -33,13 +33,16 @@ app.get('/todos', function (req, res){
 
 app.get('/todos/:id', function (req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+
+    db.todo.findById(todoId).then(function(todo){
+        if(!!todo){
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, function(e){
+        res.status(500).send();
+    });
 });
 
 app.post('/todos',function(req, res){
@@ -50,15 +53,6 @@ app.post('/todos',function(req, res){
     }, function(e){
         res.status(400).json(e);
     });
-
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-    // body.description = body.description.trim();
-
-    // body.id = todoNextId++;
-    // todos.push(body);
-    // res.json(body);
 });
 
 app.delete('/todos/:id', function (req, res){
